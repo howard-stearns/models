4a8dc62ee3112fd79e8d5a0d7ac8aeb5103d78aa
Howard Stearns <howard@highfidelity.io>
2015-11-04 08:28:50 -0800

    update

--- scripts/stress.js
+++ scripts/stress.js
@@ -1,19 +1,22 @@
 "use strict";
 /*jslint vars: true, plusplus: true*/
-/*global Agent, Avatar, Script, print*/
+/*global Agent, Avatar, Script, Entities, print*/
 
 Avatar.skeletonModelURL = "https://hifi-public.s3.amazonaws.com/marketplace/contents/ad0dffd7-f811-487b-a20a-2509235491ef/29106da1f7e6a42c7907603421fd7df5.fst?1441998287";
 Avatar.displayName = "'Bot";
 Avatar.position = {x: 500, y: 502, z: 500};
 Agent.isAvatar = true;
-//Agent.isListeningToAudioStream = true;
 
-var runtime = 0.0, count = 0;
+var ready, runtime = 0.0, count = 0;
 function update(deltaTime) {
     runtime += deltaTime;
+    if (!ready && Entities.serversExist() && Entities.canRez()) { // FIXME: also test audio and avatar mixer.
+        ready = true;
+        return;
+    }
     if (++count > 60) {
-	print('tick', runtime);
-	count = 0;
+        print('tick', runtime);
+        count = 0;
     }
 }
 print('start stress');
@@ -24,7 +27,8 @@ print('Script', JSON.stringify(Script), Script.setTimeout);
 print('before timer');
 Script.setTimeout(function () {
     print('starting update');
+    Agent.isListeningToAudioStream = true;
     Script.update.connect(update);
-    }, 10000);
+}, 10000); // until we can trust the update ready test for audio and vatar
 Script.update.connect(update);
 print('end of script file');

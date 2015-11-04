6885863a3416a0030a5c994762db3933e62939c9
Howard Stearns <howard@highfidelity.io>
2015-11-04 08:45:21 -0800

    update

--- scripts/stress.js
+++ scripts/stress.js
@@ -4,6 +4,8 @@
 
 Avatar.skeletonModelURL = "https://hifi-public.s3.amazonaws.com/marketplace/contents/ad0dffd7-f811-487b-a20a-2509235491ef/29106da1f7e6a42c7907603421fd7df5.fst?1441998287";
 Avatar.displayName = "'Bot";
+// FIXME: Position and scale.
+Avatar.scale = 1;
 Avatar.position = {x: 500, y: 502, z: 500};
 Agent.isAvatar = true;
 
@@ -14,7 +16,7 @@ function update(deltaTime) {
         ready = true;
         return;
     }
-    if (++count > 60) {
+    if (++count > (60 * 5)) {
         print('tick', runtime);
         count = 0;
     }

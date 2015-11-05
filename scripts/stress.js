e01553d6163b9b93e4a592035044dc85c4124a64
Howard Stearns <howard@highfidelity.io>
2015-11-05 13:56:16 -0800

    update

--- scripts/stress.js
+++ scripts/stress.js
@@ -4,8 +4,6 @@
 
 Avatar.skeletonModelURL = "https://hifi-public.s3.amazonaws.com/marketplace/contents/ad0dffd7-f811-487b-a20a-2509235491ef/29106da1f7e6a42c7907603421fd7df5.fst?1441998287";
 Avatar.displayName = "'Bot";
-Avatar.scale = 1;    // FIXME? Not showing up?
-Avatar.position = {x: 500, y: 502, z: 500};  // FIXME? Not showing up?
 Agent.isAvatar = true;
 
 var ready, runtime = 0.0, count = 0;
@@ -20,9 +18,11 @@ function update(deltaTime) {
         count = 0;
     }
 }
-Script.setTimeout(function () {
+Script.setTimeout(function () { // until we can trust the update ready test for audio and avatar, just wait a while
     print('starting update');
     //FIXME Agent.isListeningToAudioStream = true;
+    Avatar.scale = 1;
+    Avatar.position = {x: 500, y: 502, z: 500};
     Script.update.connect(update);
-}, 10000); // until we can trust the update ready test for audio and vatar
+}, 10000); 
 print('end of script file');

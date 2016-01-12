e2e9c62c5db4939f2084c7e51160e0c66bf3cb30
howard-stearns <howard.stearns@gmail.com>
2016-01-12 09:43:03 -0800

    less noisy  Please enter the commit message for your changes. Lines starting

--- scripts/load/entityScriptPrintUpdate.js
+++ scripts/load/entityScriptPrintUpdate.js
@@ -1,14 +1,18 @@
 "use strict";
 /*jslint nomen: true, plusplus: true, vars: true*/
 (function() {
-    function update() {
-	print('hi');
+    var accumulated = 0;
+    function update(delta) {
+	acummulated += delta;
+	if (accumulated > 5) {
+	    print('hi');
+	    accumulated = 0;
+	}
     }
     this.preload = function (entityID) { 
 	Script.update.connect(update);
     };
     this.unload = function () {
-        print('** unload');
         Script.update.disconnect(update);
     };
 })

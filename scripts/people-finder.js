05fd76c96d829b131005f3e6292b9e953391d76d
howard-stearns <howard.stearns@gmail.com>
2016-09-27 15:42:00 -0700

    indexed overlay data to support highlighting (implemented), and the start of frustum-limited display

--- scripts/people-finder.js
+++ scripts/people-finder.js
@@ -1,6 +1,6 @@
 "use strict";
 /*jslint vars: true, plusplus: true*/
-/*globals Script, AvatarList, MyAvatar, Overlays, Vec3, Toolbars, Camera, Controller, print */
+/*globals Script, AvatarList, MyAvatar, Overlays, Vec3, Toolbars, Camera, Controller, location, print */
 //
 // people-finder.js
 //
@@ -14,14 +14,23 @@
 // See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-var overlays = [];
+// FIXME: Right now, this is done with 3D overlays, with the position set where the avatar is, and
+// the size expanded for distant avatars so that the overlay is always visible in approximately constant size.
+//
+// But there are a bunch of limitations on this, such that we can't display exactly what's wanted. (See FIXMEs.)
+//
+// Maybe this would work better if we just used 2D overlay projections onto the HUD?
+
+var overlays = {};
 var ALPHA_TARGET = 0.5;
-function ExtendedOverlay(overlay) { // A wrapper around overlays to store the avatarIdentifier it is associated with.
+function ExtendedOverlay(key, overlay) { // A wrapper around overlays to store the avatarIdentifier it is associated with.
+    overlays[key] = this;
+    this.key = key;
     this.overlayIdentifier = overlay;
     this.getPosition = function () {
         // FIXME Overlays.getProperty(this.overlayIdentifier, 'position') doesn't work, so for now,
         // retain the avatarIdentifier and use that.
-        return AvatarList.getAvatar(this.avatarIdentifier).position;
+        return AvatarList.getAvatar(this.key).position;
     };
     // FIXME: I want to set the color dynamically, and I want to always drawInFront. But I can't get
     // either of these to work on model overlays. So for now, draw a model AND a sphere primitive.
@@ -30,19 +39,56 @@ function ExtendedOverlay(overlay) { // A wrapper around overlays to store the av
         alpha: ALPHA_TARGET,
         solid: true // not mesh
     });
-    this.deleteOverlay = function () {
-        Overlays.deleteOverlay(this.overlayIdentifier);
-        Overlays.deleteOverlay(this.colorOverlayIdentifier);
-    };
-    this.editOverlay = function (properties, avatarIdentifier) {
-        Overlays.editOverlay(this.overlayIdentifier, properties);
-        properties.dimensions = properties.dimensions.y; // FIXME: clicking doesn't work reliably unless I make the sphere clearly bigger than the mesh
-        Overlays.editOverlay(this.colorOverlayIdentifier, properties);
-        this.avatarIdentifier = avatarIdentifier;
-    };
-    
 }
+// Instance methods:
+ExtendedOverlay.prototype.deleteOverlay = function () {
+    Overlays.deleteOverlay(this.overlayIdentifier);
+    Overlays.deleteOverlay(this.colorOverlayIdentifier);
+    delete overlays[this.key];
+};
+ExtendedOverlay.prototype.editOverlay = function (properties) {
+    Overlays.editOverlay(this.overlayIdentifier, properties);
+    properties.dimensions = properties.dimensions.y; // FIXME: clicking doesn't work reliably unless I make the sphere clearly bigger than the mesh
+    this.color = properties.color; // FIXME Overlays.getProperty(this.colorOverlayIdentifier, 'color') doesn't work.
+    Overlays.editOverlay(this.colorOverlayIdentifier, properties);
+};
+ExtendedOverlay.prototype.highlight = function () {
+    var base = this.color;
+    var newColor = {red: base.red, green: base.green + 50, blue: base.blue};
+    Overlays.editOverlay(this.colorOverlayIdentifier, {color: newColor});
+};
+// Class methods:
+ExtendedOverlay.get = function (key) {
+    return overlays[key];
+};
+ExtendedOverlay.some = function (iterator) { // Bails early as soon as iterator returns truthy.
+    var key;
+    for (key in overlays) {
+        if (iterator(overlays[key])) {
+            return;
+        }
+    }
+};
+ExtendedOverlay.applyPickRay = function (pickRay, cb) { // cb(overlay) on the overlay intersected by pickRay, if any.
+    var pickedOverlay = Overlays.findRayIntersection(pickRay);
+    if (!pickedOverlay.intersects) {
+        return;
+    }
+    ExtendedOverlay.some(function (overlay) { // See if pickedOverlay is one of ours.
+        if (overlay.colorOverlayIdentifier == pickedOverlay.overlayID) {
+            cb(overlay);
+            return true;
+        }
+    });
+};
 
+if (!Math.sign) {
+    Math.sign = function (n) {
+        if (n > 0) { return 1; }
+        if (n < 0) { return -1; }
+        return 0;
+    };
+}
 function range(fraction, min, max) {
     return (fraction * (max - min)) + min;
 }
@@ -56,34 +102,46 @@ function colorComponent(fraction) {
 var TAN_ALPHA = 0.1;    // Arbitrary constant angular size of avatars.
 var REDUCED_TAN_ALPHA = 0.01;    // Some scaling down, but not much.
 var SCALE = 100;        // Everything past this distance is blue. Becomes red ("hot") as you get closer.
+var pingPong = false;
+var MAX_HORIZONTAL_ANGLE = 25; // degrees
+var MAX_VERTICAL_ANGLE = 10;
 function update() { // Update the overlays to align with all the other avatars.
     var avatarIdentifiers = AvatarList.getAvatarIdentifiers();
 
-    // Add missing overlays, or remove extras.
-    var deficit = avatarIdentifiers.length - 1 - overlays.length; // Don't need one for me.
-    if (deficit > 0) {
-        while (deficit-- > 0) {
-            overlays.push(new ExtendedOverlay(Overlays.addOverlay("model", {
-                drawInFront: true, // still see people who are behind walls
-                alpha: ALPHA_TARGET,
-                solid: true, // not mesh
-                url: "http://hifi-content.s3.amazonaws.com/alan/dev/Person-basemesh.fbx"
-            })));
-        }
-    } else if (deficit < 0) {
-        while (deficit++ < 0) {
-            overlays.pop().deleteOverlay();
-        }
-    }
-
     // Update the positions and colors.
-    var myPosition = MyAvatar.position, count = 0;
+    var myPosition = MyAvatar.position;
+    var viewer = Camera.orientation;
+    var forward = Quat.getFront(viewer);
+    pingPoing = !pingPong;
     avatarIdentifiers.forEach(function (identifier) {
         if (!identifier) { // Don't need one for me.
             return;
         }
-        var position = AvatarList.getAvatar(identifier).position;
-        var distance = Vec3.distance(position, myPosition);
+        var overlay = ExtendedOverlay.get(identifier);
+        if (!overlay) { // add one
+            overlay = new ExtendedOverlay(identifier, Overlays.addOverlay("model", {
+                drawInFront: true, // still see people who are behind walls
+                alpha: ALPHA_TARGET,
+                solid: true, // not mesh
+                url: "http://hifi-content.s3.amazonaws.com/alan/dev/Person-basemesh.fbx"
+            }));
+        }
+        overlay.touched = pingPong;
+        var avatar = AvatarList.getAvatar(identifier);
+        var position = avatar.position;
+        var vector = Vec3.subtract(position, myPosition);
+        var distance = Vec3.length(vector);
+        /*
+        var swing = Quat.rotationBetween(forward, Vec3.normalize(vector));
+        var decomposed = Quat.safeEulerAngles(swing);
+        var pitchUp = decomposed.x;
+        var yawLeft = decomposed.y;
+        var clampedPitch = Math.sign(pitchUp) * Math.min(Math.abs(pitchUp), MAX_VERTICAL_ANGLE);
+        var clampedYaw = Math.sign(yawLeft) * Math.min(Math.abs(yawLeft), MAX_HORIZONTAL_ANGLE);
+        var clampedSwing = Quat.fromPitchYawRollDegrees(clampedPitch, clampedYaw, decomposed.z);
+        var clampedPosition = Vec3.sum(myPosition, Vec3.multiplyQbyV(clampedSwing, Vec3.multiply(distance, forward)));
+        */
+        var clampedPosition = position;
         var fraction = Math.min(1.0, distance / SCALE);
         var principleDimension = range(1 - fraction, REDUCED_TAN_ALPHA, TAN_ALPHA) * distance;
         var secondaryDimension = principleDimension * 0.2;
@@ -92,42 +150,48 @@ function update() { // Update the overlays to align with all the other avatars.
             // FIXME: I'm sure we need the diameter to get slightly smaller with distance, so that I end up intersecting with
             // the nearest of multiple overlays that I click on. However, I'm not sure that I got this right.
             dimensions: {y: principleDimension, x: secondaryDimension, z: secondaryDimension},
-            position: position
+            position: clampedPosition
             // MAYBE FIXME: should the mesh always turn to face me?
         };
-        overlays[count++].editOverlay(properties, identifier);
+        overlay.editOverlay(properties);
+    });
+
+    // Delete any overlays that were not touched.
+    ExtendedOverlay.some(function (overlay) {
+        if (overlay.touched !== pingPong) {
+            overlay.deleteOverlay();
+        }
+    });
+
+    // See if the mouse is over any of ours, and highlight it.
+    var cursor = Reticle.position;
+    ExtendedOverlay.applyPickRay(Camera.computePickRay(cursor.x, cursor.y), function (overlay) {
+        overlay.highlight();
     });
 }
 
-function handleRay(pickRay) { // Go to the associated avatar if pickRay is to one of our overlays.
-    var index, overlay, center, vector, distance, pickedOverlay = Overlays.findRayIntersection(pickRay);
-    if (!pickedOverlay.intersects) {
-        return;
-    }
-    for (index = 0; index < overlays.length; index++) { // See if pickedOverlay is one of ours.
-        overlay = overlays[index];
-        if (overlay.colorOverlayIdentifier === pickedOverlay.overlayID) {
-            // Go to within a short distance from that overlay center -- i.e., to the other avatar.
-            // It isn't good enough to go to the pickedOverlay.intersection, because our overlay dimensions
-            // are quite large for distant overlays.
-            center = overlay.getPosition();
-            vector = Vec3.subtract(center, MyAvatar.position);
-            distance = Vec3.length(vector);
-            target = Vec3.multiply(Vec3.normalize(vector), distance - 1.0);
-            // FIXME: We would like the avatar to recompute the "maybe fly" test at the new position, so that if high enough up,
-            // the avatar goes into fly mode rather than falling. However, that is not exposed to Javascript right now.
-            // FIXME: it would be nice if this used the same teleport steps and smoothing as in the teleport.js script.
-            // Note, however, that this script allows teleporting to a person in the air, while teleport.js is going to a grounded target.
-            MyAvatar.position = Vec3.sum(MyAvatar.position, target);
-            return;
-        }
-    }
+function handleClick(pickRay) { // Go to the associated avatar if pickRay is to one of our overlays.
+    ExtendedOverlay.applyPickRay(pickRay, function (overlay) {
+        // Go to within a short distance from that overlay center -- i.e., to the other avatar.
+        // It isn't good enough to go to the pickedOverlay.intersection, because our overlay dimensions
+        // are quite large for distant overlays.
+        var center = overlay.getPosition();
+        var vector = Vec3.subtract(center, MyAvatar.position);
+        var distance = Vec3.length(vector);
+        var target = Vec3.multiply(Vec3.normalize(vector), distance - 1.0);
+        // FIXME: We would like the avatar to recompute the "maybe fly" test at the new position, so that if high enough up,
+        // the avatar goes into fly mode rather than falling. However, that is not exposed to Javascript right now.
+        // FIXME: it would be nice if this used the same teleport steps and smoothing as in the teleport.js script.
+        // Note, however, that this script allows teleporting to a person in the air, while teleport.js is going to a grounded target.
+        MyAvatar.position = Vec3.sum(MyAvatar.position, target);
+        return true;
+    });
 }
 function handleMouseEvent(event) {
     if (!event.isLeftButton) {
         return;
     }
-    handleRay(Camera.computePickRay(event.x, event.y));
+    handleClick(Camera.computePickRay(event.x, event.y));
 }
 // FIXME: This also needs to work with laser triggering, not just mouse!
 
@@ -159,7 +223,7 @@ function clearExpiration() {
 function startSearch() {
     clearExpiration();
     // FIXME: normal Script.update is too fast for weaker hardware.
-    ticker = Script.setInterval(update, 180);
+    ticker = Script.setInterval(update, 60); //FIXME 180);
     Controller.mousePressEvent.connect(handleMouseEvent);
     setButtonState(ON_STATE);
 }
@@ -171,9 +235,9 @@ function stopSearch() {
         Controller.mousePressEvent.disconnect(handleMouseEvent);
     }
     setButtonState(OFF_STATE);
-    while (overlays.length) {
-        overlays.pop().deleteOverlay();
-    }
+    ExtendedOverlay.some(function (overlay) {
+        overlay.deleteOverlay();
+    });
 }
 function toggleSearch() {
     if (ticker) {
